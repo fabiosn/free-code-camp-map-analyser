691b75e139f01c4e2649eb316bb1017c5f83b717
fabio <fabio.queluci@gmail.com>
2017-02-07 10:40:57 -0200

    Remove the map.html file synchronously and write it again based on a flag passed to the script

--- map-analyser.js
+++ map-analyser.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
 const request = require('request');
 
-if (fs.exists('map.html')) {
-  fs.unlink('map.html');
-}
+if (process.argv.indexOf('-f')) {
+  if (fs.exists('map.html')) {
+    fs.unlinkSync('map.html');
+  }
 
-request('http://freecodecamp.com/map', function(error, response, body) {
   console.log('Getting data...');
-})
-.pipe(fs.createWriteStream('map.html'));
+
+  request('http://freecodecamp.com/map')
+    .pipe(
+      fs.createWriteStream('map.html')
+      .on('close', function() {
+        console.log('Done.');
+      })
+    );
+}
